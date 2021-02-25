98e742d5122bc0a4cdb0bc8a49206cfc0df52d4d
anastazia17 <stasya17kolomna@gmail.com>
2021-02-25 13:44:52 +0300

    Change file urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index
-from mainapp.views import products
-from mainapp.views import contact
-from mainapp.views import test_context
+from mainapp.views import index, products, contact, test_context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
