518e7b246c123e7eac14acf7966ed5f043c91501
anastazia17 <stasya17kolomna@gmail.com>
2021-03-03 23:56:02 +0300

    Change file urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -22,9 +22,9 @@ from mainapp.views import index, products, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('products/', products),
-    path('contact/', contact),
+    path('', index, name='index'),
+    path('products/', products, name='products'),
+    path('contact/', contact, name='contact'),
 ]
 
 if settings.DEBUG:
