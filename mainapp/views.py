110d07f4b6b7ba45cafacba1fae6035b0bdfa428
anastazia17 <stasya17kolomna@gmail.com>
2021-03-03 23:18:13 +0300

    Correct models in files products.html and views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,22 @@
+import os
+import json
+
 from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
+
+dir = os.path.dirname(__file__)
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title':'GeekShop',
+               'products': Product.objects.all(),
+               'categories':ProductCategory.objects.all()}
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {'title': 'GeekShop - Каталог'}
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {'title': 'GeekShop - Контакты'}
+    return render(request, 'mainapp/contact.html', context)
