9444851e0b34021748b34aec007297fb279ea9d4
anastazia17 <stasya17kolomna@gmail.com>
2021-03-13 02:22:19 +0300

    Add button 'Войти' finish working of login and register

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render (request, 'authapp/login.html')
+
+def register(request):
+    return render (request, 'authapp/register.html')
+
+def profile(request):
+    return render (request, 'authapp/profile.html')
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -24,9 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
-    path('products/', include('mainapp.urls', namespace='products'))
-
-    #path('products/', products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
 
 if settings.DEBUG:
